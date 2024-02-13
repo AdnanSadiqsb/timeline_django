897c7172f49007851d83468878c4540abfbe1f7b
adnan sadiq <adnansadiqxyz@gmail.com>
2024-02-13 22:06:46 +0500

    static files updates

--- timeline/settings.py
+++ timeline/settings.py
@@ -131,10 +131,5 @@ AUTHENTICATION_BACKENDS = ["project.backends.EmailBackend"]  # updated
 
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-
-STATIC_URL = "static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
